b306c84581d183d01adf234294e5c3261f2f541d
DASKALELIS_Emmanouil <Manolisdaskalelis@gmail.com>
2026-06-18 15:38:57 +0300

    My account in progress

--- yap-spot-client/src/app/layout/settings/my-account-settings/my-account/my-account.component.ts
+++ yap-spot-client/src/app/layout/settings/my-account-settings/my-account/my-account.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-account',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-account.component.html',
   styleUrl: './my-account.component.css',
 })
 export class MyAccountComponent {
-
+  accountItems = [
+    {
+      key: 'my-account',
+      label: 'Display Name',
+      secondLabel: 'manos_d',
+      icon: 'pi-user',
+      btnLabel: 'Edit',
+    },
+    {
+      key: 'email',
+      label: 'Email',
+      secondLabel: 'manos@yap.com',
+      icon: 'pi-envelope',
+      btnLabel: 'Edit',
+    },
+    {
+      key: 'password',
+      label: 'Password',
+      secondLabel: 'Last changed 3 months ago',
+      icon: 'pi-lock',
+      btnLabel: 'Change',
+    },
+    {
+      key: 'auth',
+      label: 'Two-factor Auth',
+      secondLabel: 'Enabled',
+      icon: 'pi-shield',
+      btnLabel: 'Manage',
+    },
+  ];
 }
